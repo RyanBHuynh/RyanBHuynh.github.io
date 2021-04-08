a65a86f0a5531a574b53d9c9b2113fbc30ad2cc5
Ryan Huynh <ryanhuynh@Ryans-MacBook-Air.local>
2021-04-08 17:38:06 -0500

    add pictures for big o article

---
+++ files/Big O Notation Article/Code For Big O Article.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int foo = 1; 
+    int bar = 99999; 
+
+    int i = 0;
+    int sum = 0;
+
+    while(i < n)
+    {
+        sum += array[i];
+        i++;
+    }
+}
+
